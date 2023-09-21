4edbe24103b02a0514a6634b759a71bcffa5ec3b
Brian Gyss <bgyss@hey.com>
2023-09-21 22:55:41 +0000

    added sample main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+torch.set_default_device('cuda')
+model = AutoModelForCausalLM.from_pretrained("microsoft/phi-1_5", 
+                                             trust_remote_code=True, torch_dtype="auto")
+tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-1_5", 
+                                          trust_remote_code=True, torch_dtype="auto")
+inputs = tokenizer('''```python
+def print_prime(n):
+   """
+   Print all primes between 1 and n
+   """''', return_tensors="pt", return_attention_mask=False)
+
+outputs = model.generate(**inputs, max_length=200)
+text = tokenizer.batch_decode(outputs)[0]
+print(text)
